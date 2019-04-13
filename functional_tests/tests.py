d23a357b72029a39f50fa9be38f1aa79f3dda371
Monkey-D-Luffy <530659039@qq.com>
2019-04-13 18:51:38 +0800

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
         self.browser.get(self.live_server_url)
@@ -65,7 +65,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Edith wonders whether the site will remember her list. Then she sees that the site
         # has generated a unique URL for her -- there is some explanatory text to that effect.
-        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
